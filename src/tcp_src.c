e6f3a996b70d9107cb0211146c1ca8ecf529c9d1
Kristian Larsson <kristian@spritelink.net>
2023-09-04 17:30:08 +0200

    Add missing header includes

--- src/tcp_src.c
+++ src/tcp_src.c
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#include <stdlib.h>
 
 #include "tlsuv/tcp_src.h"
 #include "tlsuv/http.h"
